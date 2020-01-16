ccfd445f7e41747c893a89fd7bfd011cfd05cc77
ajedelmann <albert.edelmann@gmail.com>
2020-01-16 20:25:07 +0100

    Updates

---
+++ precache-manifest.6fb048683776fe4689a762dfddff1657.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "266b4552af8297a31e84d64169640a96",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "53b149bbc8c3bf76c3b3",
+    "url": "/portfolio/static/css/main.d6aa73ec.chunk.css"
+  },
+  {
+    "revision": "0a42821cdb8e4123113f",
+    "url": "/portfolio/static/js/2.60f6c105.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.60f6c105.chunk.js.LICENSE"
+  },
+  {
+    "revision": "53b149bbc8c3bf76c3b3",
+    "url": "/portfolio/static/js/main.950167de.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "11b48ce4966eaa6cac8cca3a47d3bb32",
+    "url": "/portfolio/static/media/darth-vader.11b48ce4.png"
+  },
+  {
+    "revision": "0d550526602c7bb1c64ca8a50338bb4c",
+    "url": "/portfolio/static/media/ear.0d550526.jpeg"
+  },
+  {
+    "revision": "c75fc36259f45eb7573a3ca16fdc2be1",
+    "url": "/portfolio/static/media/jammming.c75fc362.jpeg"
+  },
+  {
+    "revision": "ab57af58367326230403b2743b8ba949",
+    "url": "/portfolio/static/media/piano-player.ab57af58.jpeg"
+  },
+  {
+    "revision": "5a958a65cca4c448b8851202c5897e1a",
+    "url": "/portfolio/static/media/ravenous.5a958a65.jpeg"
+  },
+  {
+    "revision": "3f20d08ef827b5f6248b68b66e9f5bba",
+    "url": "/portfolio/static/media/totizeke.3f20d08e.png"
+  }
+]);
