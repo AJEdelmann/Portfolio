b76b568df29b4d3558b35416c9fcae9813f27929
Albert Edelmann <albert.edelmann@gmail.com>
2022-09-25 12:41:41 +0200

    improved hero header mobile view

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import MyPhoto from "../img/totizeke.png";
 const Home = () => {
   return (
     <div className="home">
-      <div className="col4 myText">
+      <div className="myText">
         <h1 id="myName">
           <span className="aert">A</span>
           <span className="lbj">LB</span>
@@ -18,7 +18,7 @@ const Home = () => {
         <h2 className="myWebDev">Software Engineer</h2>
         <h3 className="myBerlin">based in Berlin, Germany</h3>
       </div>
-      <div className="col8 photoDiv">
+      <div className="photoDiv">
         <img src={MyPhoto} alt="Albert" className="myPhoto" loading="lazy"></img>
       </div>
     </div>
